2bf515973d04fd474a0a3485df268127947b7bb5
Ubuntu <ubuntu@ip-172-31-17-2.ca-central-1.compute.internal>
2017-05-03 10:35:38 +0000

    added basic user authentication (ion auth)

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class Dashboard extends Auth_Controller {
+ 
+    public function index()
+    {
+        echo 'Hello from the dashboard';
+    }
+}
+?>
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,59 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class MY_Controller extends CI_Controller {
+ 
+    protected $data = array();
+    function __construct()
+    {
+        log_message("debug", "enter MY_Controller:construct method");
+        parent::__construct();
+        $this->data['page_title'] = 'CI App';
+        $this->data['page_description'] = 'CI_App';
+        $this->data['before_closing_head'] = '';
+        $this->data['before_closing_body'] = '';
+    }
+ 
+    protected function render($the_view = NULL, $template = 'public_master')
+    {
+        log_message("debug", "enter MY_Controller:render method");
+ 
+        if($template == 'json' || $this->input->is_ajax_request())
+        {
+            header('Content-Type: application/json');
+            echo json_encode($this->data);
+        }
+        elseif(is_null($template))
+        {
+            $this->load->view($the_view,$this->data);
+        }
+        else
+        {
+            $this->data['the_view_content'] = (is_null($the_view)) ? '' : $this->load->view($the_view, $this->data, TRUE);
+            $this->load->view('templates/' . $template . '_view', $this->data);
+        }
+    }
+}
+
+class Auth_Controller extends MY_Controller {
+    function __construct() {
+        parent::__construct();
+
+        log_message("debug", "enter Auth_Controller:construct method");
+ 
+
+        $this->load->library('ion_auth');
+        if($this->ion_auth->logged_in()===FALSE)
+        {
+            redirect('users/login');
+        }
+    }
+    protected function render($the_view = NULL, $template = 'auth_master')
+    {
+        parent::render($the_view, $template);
+        log_message("debug", "enter Auth_Controller:render method");
+ 
+    }
+}
+
+?>
